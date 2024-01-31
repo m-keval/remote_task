67a17ab4c1079d6556ca9bde8f44f0d5b8a2b03f
Keval <mkeval2306@gmail.com>
2024-01-31 21:19:36 +0530

    Task Done

---
+++ lib/painters/eye_painter.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/painting.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+class EyePainter extends CustomPainter {
+  final ui.Image image;
+  final List<Face> faces;
+  final List<Rect> rects = [];
+
+  EyePainter(this.image, this.faces) {
+    for (var i = 0; i < faces.length; i++) {
+      rects.add(faces[i].boundingBox);
+    }
+  }
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    void drawEye(Canvas canvas, Point point) {
+      Paint paintEye = Paint()
+        ..color = Colors.green.withOpacity(0.5)
+        ..style = PaintingStyle.fill;
+
+      Offset offset = Offset(point.x.toDouble() - 30, point.y.toDouble() - 15);
+      Rect rect =
+          Rect.fromPoints(offset, Offset(offset.dx + 60, offset.dy + 30));
+      canvas.drawOval(rect, paintEye);
+    }
+
+    // canvas.drawImage(image, Offset.zero, Paint());
+    for (var i = 0; i < faces.length; i++) {
+      if (faces[i].landmarks[FaceLandmarkType.leftEye] != null &&
+          faces[i].landmarks[FaceLandmarkType.rightEye] != null) {
+        drawEye(canvas, faces[i].landmarks[FaceLandmarkType.leftEye]!.position);
+        drawEye(
+            canvas, faces[i].landmarks[FaceLandmarkType.rightEye]!.position);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(EyePainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
+  }
+}
--- lib/painters/mouth_painter.dart
+++ lib/painters/mouth_painter.dart
@@ -1,17 +1,14 @@
 import 'dart:math';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/painting.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 
-class EyePainter extends CustomPainter {
+class MouthPainter extends CustomPainter {
   final ui.Image image;
   final List<Face> faces;
   final List<Rect> rects = [];
 
-
-  EyePainter(this.image, this.faces) {
+  MouthPainter(this.image, this.faces) {
     for (var i = 0; i < faces.length; i++) {
       rects.add(faces[i].boundingBox);
     }
@@ -19,31 +16,30 @@ class EyePainter extends CustomPainter {
 
   @override
   void paint(ui.Canvas canvas, ui.Size size) {
-
-    void drawEye(Canvas canvas, Point point) {
-      Paint paintEye = Paint()
-        ..color = Colors.red.withOpacity(0.5)
+    void drawMouth(Canvas canvas, Point point) {
+      Paint paintMouth = Paint()
+        ..color = Colors.green.withOpacity(0.5)
         ..style = PaintingStyle.fill;
 
-      Offset offset = Offset(point.x.toDouble() - 30, point.y.toDouble() - 15);
-      Rect rect =
-      Rect.fromPoints(offset, Offset(offset.dx + 60, offset.dy + 30));
-      canvas.drawOval(rect, paintEye);
+      //mouth Offset
+      Offset mouthOffset =
+          Offset(point.x.toDouble() - 50, point.y.toDouble() - 35);
+      Rect mouthRect = Rect.fromPoints(
+          mouthOffset, Offset(mouthOffset.dx + 100, mouthOffset.dy + 40));
+      canvas.drawOval(mouthRect, paintMouth);
     }
 
-    canvas.drawImage(image, Offset.zero, Paint());
     for (var i = 0; i < faces.length; i++) {
-      if (faces[i].landmarks[FaceLandmarkType.leftEye] != null &&
-          faces[i].landmarks[FaceLandmarkType.rightEye] != null) {
-        drawEye(canvas, faces[i].landmarks[FaceLandmarkType.leftEye]!.position);
-        drawEye(
-            canvas, faces[i].landmarks[FaceLandmarkType.rightEye]!.position);
+      if (faces[i].landmarks[FaceLandmarkType.bottomMouth] != null) {
+        drawMouth(
+            canvas, faces[i].landmarks[FaceLandmarkType.bottomMouth]!.position);
       }
+      //canvas.drawRect(rects[i], paint);
     }
   }
 
   @override
-  bool shouldRepaint(EyePainter old) {
-    return image != old.image || faces != old.faces;
+  bool shouldRepaint(MouthPainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
   }
 }
